06677e4d66854ed86239b6d18d42f9e9fb4e72a4
Tung Vu <tungvu@adatau.com>
2015-07-28 18:28:59 +0700

    Add filter for nunjucks to render json string from obj

--- app/server.js
+++ app/server.js
@@ -1,26 +1,28 @@
 'use strict';
 
-import koa                  from 'koa';
-import logger               from 'koa-logger';
-import serveStatic          from 'koa-static';
-import router               from 'koa-route';
-import nunjucks             from 'nunjucks';
-import serverSettings       from './server/settings';
-import serverStatic         from './server/serve-static';
-import serverNunjucksConfig from './server/nunjucks-config';
+import koa from 'koa';
+import logger from 'koa-logger';
+import serveStatic from 'koa-static';
+import router from 'koa-route';
+import nunjucks from 'nunjucks';
+import serverSettings from './server/settings';
 
 const app = koa();
 
 // settings
 app.use(logger());
 app.use(router.get('/assets', serveStatic('./public/assets')));
-nunjucks.configure('views', { autoescape: true });
+let nunjucksEnv = nunjucks.configure('views', {
+  autoescape: true
+});
+
+nunjucksEnv.addFilter('json', obj => JSON.stringify(obj));
 
 // response
-// app.use(function *(){
-//   console.log(serverSettings);
-//   this.body = nunjucks.render('application/index.html', serverSettings);
-// });
+app.use(function* () {
+  console.log(serverSettings);
+  this.body = nunjucks.render('application/index.html', serverSettings);
+});
 
 app.listen(3000);
 console.log('Server listening on port 3000');
--- config/server/development.js
+++ config/server/development.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var _             = require('lodash');
-var path          = require('path');
-var webpack       = require('webpack');
+var _ = require('lodash');
+var path = require('path');
+var webpack = require('webpack');
 var defaultConfig = require('./default');
 
 module.exports = _.merge(defaultConfig, {
@@ -26,8 +26,11 @@ module.exports = _.merge(defaultConfig, {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(), new webpack.NoErrorsPlugin(), // Hot Module Replacement
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"', 'process.env.NO_SERVER_RENDERING': true })
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"',
+      'process.env.NO_SERVER_RENDERING': true
+    })
   ]
-}, function(obj1, obj2) {
+}, function (obj1, obj2) {
   return _.isArray(obj2) ? obj2.concat(obj1) : undefined;
 });
