2a12f9decd4af4cc5214eb98b79aad2ced498202
hung-phan <colorvisavn@gmail.com>
2015-07-30 09:42:43 +0700

    update template

--- app/client/lib/index.js
+++ app/client/lib/index.js
@@ -3,6 +3,7 @@
 // expose to global env
 require('expose?jQuery!expose?$!jquery');
 require('expose?React!react/addons');
+// require('imports?this=>window!exports?window.Modernizr!modernizr/modernizr');
 
 // load libs
 require('./twitter-bootstrap');
--- app/server.js
+++ app/server.js
@@ -8,10 +8,13 @@ import nunjucks       from 'nunjucks';
 import nunjucksEnv    from './server/nunjucks-settings';
 import serverSettings from './server/settings';
 
+const ROOT_PATH   = path.join(__dirname, './../');
+const PUBLIC_PATH = path.join(ROOT_PATH, 'public');
 const PORT = 3000;
 const app = koa();
 
 // settings
+app.use(middlewares.favicon(path.join(PUBLIC_PATH, 'favicon.ico')));
 app.use(middlewares.bodyParser());
 app.use(middlewares.logger());
 app.use(middlewares.compress());
@@ -20,9 +23,7 @@ app.use(htmlMinifier({
   removeComments: true,
   preserveLineBreaks: process.env.NODE_ENV === 'development'
 }));
-app.use(middlewares.staticCache(
-  path.join(__dirname, './../public'), { gzip: true }
-));
+app.use(middlewares.staticCache(PUBLIC_PATH, { gzip: true }));
 middlewares.csrf(app);
 app.use(middlewares.csrf.middleware);
 
