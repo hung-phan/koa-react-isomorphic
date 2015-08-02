aab7bd21978eefe6b36a6f8b48fc22624e12e919
Bao Nguyen <b@nqbao.com>
2015-08-02 18:33:29 +0700

    WIP: move nunjucks to initializers. Expose it back as a middleware

--- config/initializers/nunjucks.js
+++ config/initializers/nunjucks.js
@@ -1,11 +1,29 @@
 'use strict';
 
+import koa               from 'koa';
+import nunjucks          from 'nunjucks';
+import settingsConfig    from './settings';
+
 const options = {
   autoescape: true
 };
 
-export default function(nunjucks) {
+export default function(app : koa) {
   const env = nunjucks.configure('app/server/templates', options);
 
   env.addFilter('json', JSON.stringify);
+
+  app.use(function* views(next) {
+    if (!this.render) {
+      this.render = function(template, parameters) {
+        parameters = parameters || {};
+
+        return nunjucks.render(template, {
+          ...parameters, ...settingsConfig, csrf: this.csrf
+        });
+      };
+    }
+
+    yield next;
+  });
 }
