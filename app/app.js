a9bbd32117319ce1ef8df1067e3f29b5d84de16a
hung-phan <colorvisavn@gmail.com>
2015-08-04 08:56:50 +0700

    add enhance component

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ import $      from 'jquery';
 import React  from 'react/addons';
 import Router from 'react-router';
 import Home   from './client/components/home/home';
+import Enhance   from './client/components/enhance/enhance';
 
 $(document).ready(function() {
   const routes = (
