49206f69728149185a93be6106a7034ebb7f507f
hung-phan <colorvisavn@gmail.com>
2015-08-27 01:11:52 -0700

    update state

--- app/client/stores/index-test.js
+++ app/client/stores/index-test.js
@@ -2,15 +2,37 @@ import { assert }     from 'chai';
 import configureStore from './index';
 
 describe('App state', () => {
-  const store = configureStore();
-  const state = store.getState();
+  describe('with empty state', () => {
+    const store = configureStore();
+    const state = store.getState();
 
-  it('should be defined', () => {
-    assert.ok(state);
+    it('should be defined', () => {
+      assert.ok(state);
+    });
+
+    it('should define todos property', () => {
+      assert.ok(state.todos);
+      assert.deepEqual(state.todos, []);
+    });
   });
 
-  it('should define todos property', () => {
-    assert.ok(state.todos);
-    assert.deepEqual(state.todos, []);
+  describe('with given state', () => {
+    const store = configureStore({
+      todos: [
+        { text: 'do chore', complete: true }
+      ]
+    });
+    const state = store.getState();
+
+    it('should be defined', () => {
+      assert.ok(state);
+    });
+
+    it('should define todos property', () => {
+      assert.ok(state.todos);
+      assert.deepEqual(state.todos, [
+        { text: 'do chore', complete: true }
+      ]);
+    });
   });
 });
