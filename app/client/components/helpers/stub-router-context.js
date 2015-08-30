8b97c204682131e7ee77da8932927766f9fa5d44
hung-phan <colorvisavn@gmail.com>
2015-08-29 22:58:59 -0700

    add stub-router-context to istanbul ignore

--- app/client/components/helpers/stub-router-context.js
+++ app/client/components/helpers/stub-router-context.js
@@ -1,6 +1,6 @@
-import 'babel/polyfill';
 import React from 'react/addons';
 
+/* istanbul ignore next */
 export default function(ComposedComponent, props = {}, stubs = {}) {
   function RouterStub() { }
 
