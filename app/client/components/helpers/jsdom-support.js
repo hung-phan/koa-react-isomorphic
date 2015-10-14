2cf7c2f681785804ca1d9ead6790fceb37ebfc2e
hung-phan <colorvisavn@gmail.com>
2015-10-14 15:32:53 +0700

    Remove ExecutionEnvironment

--- app/client/components/helpers/jsdom-support.js
+++ app/client/components/helpers/jsdom-support.js
@@ -1,8 +1,4 @@
 import { jsdom } from 'jsdom';
-import ExecutionEnvironment from 'react/lib/ExecutionEnvironment';
-
-// config React DOM
-ExecutionEnvironment.canUseDOM = true;
 
 if (typeof document === 'undefined') {
   global.document = jsdom('<html><body></body></html>');
