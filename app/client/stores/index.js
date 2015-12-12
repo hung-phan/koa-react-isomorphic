5b45173b1cc97451407691d6f4a4ffb66c85e98b
hung-phan <colorvisavn@gmail.com>
2015-12-12 22:41:30 +0700

    Update deprecated transformer to stateTransformer

--- app/client/stores/index.js
+++ app/client/stores/index.js
@@ -11,7 +11,7 @@ let developmentMiddlewares = [];
 if (process.env.NODE_ENV === 'development' && !process.env.SERVER_RENDERING) {
   const logger = require('redux-logger')({
     level: 'info',
-    transformer: state => state.toJS()
+    stateTransformer: state => state.toJS()
   });
 
   middlewares = [logger, ...middlewares];
