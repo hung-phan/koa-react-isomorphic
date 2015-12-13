fc7fdd7bf66940ef3fb41f0b1c759211c9b3ec3c
hung-phan <colorvisavn@gmail.com>
2015-12-13 11:02:13 +0700

    Work in progress

--- config/middlewares/custom/render.js
+++ config/middlewares/custom/render.js
@@ -1,19 +1,19 @@
 import nunjucks from 'nunjucks';
 import settings from 'config/initializers/settings';
 
-export default function* (next) {
-  this.render = this.render ||
-    function(template: string, parameters: Object = {}) {
-      return new Promise(resolve => {
-        resolve(
-          nunjucks.render(template, {
-            ...settings,
-            ...parameters,
-            csrf: this.csrf
-          })
-        );
-      });
-    };
+function render(template: string, parameters: Object = {}) {
+  return new Promise(resolve => {
+    resolve(
+      nunjucks.render(template, {
+        ...settings,
+        ...parameters,
+        csrf: this.csrf
+      })
+    );
+  });
+};
 
+export default function*(next) {
+  this.render = this.render || render;
   yield next;
 }
