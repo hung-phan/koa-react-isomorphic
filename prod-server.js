8dc3afc958d3e82240a4527a94007b2cea8ca0bf
hung-phan <colorvisavn@gmail.com>
2016-01-10 14:00:52 +0700

    Update production server

--- prod-server.js
+++ prod-server.js
@@ -1,13 +1,13 @@
-var path = require('path');
-var ROOT = path.join(__dirname);
-var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
+const path = require('path');
+const ROOT = path.join(__dirname);
+const WebpackIsomorphicTools = require('webpack-isomorphic-tools');
 
 global.webpackIsomorphicTools = new WebpackIsomorphicTools(
   require('config/webpack/webpack-isomorphic-tools')
 );
 
-webpackIsomorphicTools
+global.webpackIsomorphicTools
   .development(process.env.NODE_ENV === 'development')
-  .server(ROOT, function() {
+  .server(ROOT, () => {
     require('./build/server');
   });
