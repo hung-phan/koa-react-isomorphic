ef63c684628d808446e188612780babec71bb352
hung-phan <colorvisavn@gmail.com>
2016-01-10 14:09:38 +0700

    Update stub router and provider mock

--- app/client/components/test-helpers/stub-router-context.js
+++ app/client/components/test-helpers/stub-router-context.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 /* istanbul ignore next */
-export default function(ComposedComponent, props = {}, stubs = {}) {
+export default (ComposedComponent, props = {}, stubs = {}) => {
   function RouterStub() { }
 
   Object.assign(RouterStub, {
-    makePath () {},
-    makeHref () {},
-    transitionTo () {},
-    replaceWith () {},
-    goBack () {},
-    getCurrentPath () {},
-    getCurrentRoutes () {},
-    getCurrentPathname () {},
-    getCurrentParams () {},
-    getCurrentQuery () {},
-    isActive () {},
+    makePath() {},
+    makeHref() {},
+    transitionTo() {},
+    replaceWith() {},
+    goBack() {},
+    getCurrentPath() {},
+    getCurrentRoutes() {},
+    getCurrentPathname() {},
+    getCurrentParams() {},
+    getCurrentQuery() {},
+    isActive() {},
     getRouteAtDepth() {},
     setRouteComponentAtDepth() {}
   }, stubs);
@@ -24,16 +24,16 @@ export default function(ComposedComponent, props = {}, stubs = {}) {
     static childContextTypes = {
       router: React.PropTypes.func,
       routeDepth: React.PropTypes.number
-    }
+    };
 
-    getChildContext () {
+    getChildContext() {
       return {
         router: RouterStub,
         routeDepth: 0
       };
     }
 
-    render () {
+    render() {
       return (
         <ComposedComponent {...props} />
       );
