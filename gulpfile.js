8cd8e65aadaed2a8b6c131c9c41cd18a1f21b8b1
hung-phan <colorvisavn@gmail.com>
2016-01-10 13:34:37 +0700

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,90 @@
 'use strict';
 
-var path             = require('path');
-var _                = require('lodash');
-var fs               = require('fs');
-var del              = require('del');
-var gulp             = require('gulp');
-var gutil            = require('gulp-util');
-var env              = require('gulp-env');
-var webpack          = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var notifier         = require('node-notifier');
-var shell            = require('shelljs');
-var config           = require('config/config.json');
+const del = require('del');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const env = require('gulp-env');
+const webpack = require('webpack');
+const shell = require('shelljs');
+const config = require('config/config.json');
 
-gulp.task('frontend:watch', function(done) {
-  shell.exec('npm run frontend:watch', function() {
+// clean task
+function cleanTask(files) {
+  return () => {
+    del(files, (err, paths) => {
+      gutil.log('Deleted files/folders:\n', gutil.colors.cyan(paths.join('\n')));
+    });
+  };
+}
+
+gulp.task('frontend:watch', (done) => {
+  shell.exec('npm run frontend:watch', () => {
     done();
   });
 });
 
-gulp.task('frontend:build', ['set-production-env'], function(done) {
-  var bundler = webpack(require('config/webpack/client/production')),
-      handler = function (err, stats) {
-                  if (err) {
-                    notifier.notify({ message: 'Error: ' + err.message });
-                    throw new gutil.PluginError('webpack', err);
-                  }
-
-                  gutil.log('[webpack]', stats.toString({colors: true}));
-                  done();
-                };
-
-  bundler.run(handler);
-});
-
-gulp.task('backend:watch', function(cb) {
-  var started = false;
-
-  webpack(require('config/webpack/server/development')).watch(100, function(err, stats) {
-    if(!started) {
-      started = true;
-      cb();
+gulp.task('frontend:build', ['set-production-env'], (done) => {
+  const bundler = webpack(require('config/webpack/client/production'));
+  const handler = (err, stats) => {
+    if (err) {
+      throw new gutil.PluginError('webpack', err);
     }
 
-    gutil.log('[webpack]', stats.toString({colors: true}));
-  });
-});
-
-gulp.task('backend:build', ['set-production-env'], function(done) {
-  var bundler = webpack(require('config/webpack/server/production')),
-      handler = function (err, stats) {
-        if (err) {
-          notifier.notify({ message: 'Error: ' + err.message });
-          throw new gutil.PluginError('webpack', err);
-        }
-
-        gutil.log('[webpack]', stats.toString({colors: true}));
-      };
+    gutil.log('[webpack]', stats.toString({ colors: true }));
+    done();
+  };
 
   bundler.run(handler);
 });
 
-gulp.task('set-production-env', function() {
+gulp.task('backend:watch', (done) => {
+  let started = false;
+
+  webpack(require('config/webpack/server/development'))
+    .watch(100, (err, stats) => {
+      if (!started) {
+        started = true;
+        done();
+      }
+
+      gutil.log('[webpack]', stats.toString({ colors: true }));
+    });
+});
+
+gulp.task('backend:build', ['set-production-env'], (done) => {
+  const bundler = webpack(require('config/webpack/server/production'));
+  const handler = function (err, stats) {
+    if (err) {
+      throw new gutil.PluginError('webpack', err);
+    }
+
+    gutil.log('[webpack]', stats.toString({ colors: true }));
+    done();
+  };
+
+  bundler.run(handler);
+});
+
+gulp.task('set-production-env', () => {
   env({
-    vars: {
+    consts: {
       NODE_ENV: 'production'
     }
   });
 });
 
-gulp.task('clean', cleanTask(['.' + config.path.build, '.' + config.path.publicAssets]));
+gulp.task('clean', cleanTask([`.${config.path.build}`, `.${config.path.publicAssets}`]));
 gulp.task('watch', ['clean', 'frontend:watch', 'backend:watch']);
 gulp.task('build', ['clean', 'frontend:build', 'backend:build']);
 
-gulp.task('pro-server', function(done) {
-  shell.exec('pm2 start config/pm2/production.json', function() {
+gulp.task('pro-server', (done) => {
+  shell.exec('pm2 start config/pm2/production.json', () => {
     done();
   });
 });
 
-gulp.task('pro-server-delete', function(done) {
-  shell.exec('pm2 delete config/pm2/production.json', function() {
+gulp.task('pro-server-delete', (done) => {
+  shell.exec('pm2 delete config/pm2/production.json', () => {
     done();
   });
 });
-
-// clean task
-function cleanTask(files) {
-  return function() {
-    del(files, function (err, paths) {
-      gutil.log('Deleted files/folders:\n', gutil.colors.cyan(paths.join('\n')));
-    });
-  };
-}
