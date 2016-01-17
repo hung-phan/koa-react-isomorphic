8362090b4a21d8e3a2b115086f9f2d02fc1c6511
hung-phan <colorvisavn@gmail.com>
2016-01-17 18:27:51 +0700

    Update style

--- app/database/schema.js
+++ app/database/schema.js
@@ -1,35 +1,22 @@
-/* eslint-disable */
-import  _ from 'lodash';
+import _ from 'lodash';
 
 // This is the Dataset in our blog
-import { PostsList, AuthorsList, CommentList, ReplyList } from './database';
+import { PostsList, AuthorsList } from './database';
 
 import {
-  // These are the basic GraphQL types
-  GraphQLInt,
-  GraphQLFloat,
   GraphQLString,
   GraphQLList,
   GraphQLObjectType,
-  GraphQLEnumType,
-
-  // This is used to create required fileds and arguments
   GraphQLNonNull,
-
-  // This is the class we need to create the schema
   GraphQLSchema,
 } from 'graphql';
-
-/**
-  DEFINE YOUR TYPES BELOW
-**/
 
 const Author = new GraphQLObjectType({
   name: 'Author',
   description: 'This represent an author',
   fields: () => ({
-    _id: {type: new GraphQLNonNull(GraphQLString)},
-    name: {type: GraphQLString}
+    _id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: GraphQLString }
   })
 });
 
@@ -37,13 +24,13 @@ const Post = new GraphQLObjectType({
   name: 'Post',
   description: 'This represent a Post',
   fields: () => ({
-    _id: {type: new GraphQLNonNull(GraphQLString)},
-    title: {type: new GraphQLNonNull(GraphQLString)},
-    content: {type: GraphQLString},
+    _id: { type: new GraphQLNonNull(GraphQLString) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: GraphQLString },
     author: {
       type: Author,
-      resolve: function(post) {
-        return _.find(AuthorsList, a => a._id == post.author);
+      resolve(post) {
+        return _.find(AuthorsList, a => a._id === post.author);
       }
     }
   })
@@ -56,24 +43,13 @@ const Query = new GraphQLObjectType({
   fields: () => ({
     posts: {
       type: new GraphQLList(Post),
-      resolve: function() {
+      resolve() {
         return PostsList;
-      }
-    },
-    echo: {
-      type: GraphQLString,
-      description: 'Echo what you enter',
-      args: {
-        message: {type: GraphQLString}
-      },
-      resolve: function(source, {message}) {
-        return {aa: 10};
       }
     }
   })
 });
 
-// This the Schema
 const Schema = new GraphQLSchema({
   query: Query
 });
