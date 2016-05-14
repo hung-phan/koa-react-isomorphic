9bb9701bc23038ef6934fa3ff1e2f9e4b7c5eb7c
hung-phan <colorvisavn@gmail.com>
2016-05-14 01:10:23 -0700

    Update FetchDataEnhancer

--- app/client/helpers/fetch-data-enhancer.js
+++ app/client/helpers/fetch-data-enhancer.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { provideHooks } from 'redial';
 
-export default (callback) => (ComposedComponent) => {
-  class FetchDataEnhancer extends ComposedComponent {
-    render() {
-      return (
-        <ComposedComponent { ...this.props } />
-      );
-    }
-  }
-
-  return provideHooks({
+export default (callback) => (ComposedComponent) =>
+  provideHooks({
     fetchData(...args) {
       return callback(...args);
     },
-  })(FetchDataEnhancer);
-};
+  })(
+    props => <ComposedComponent { ...props } />
+  );
