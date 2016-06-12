b40a1461e33a45b83831ecb6ff3cda4ee1f8e550
hung-phan <colorvisavn@gmail.com>
2016-06-12 23:56:02 +0700

    Update dependencies

--- app/client/lib/index.js
+++ app/client/lib/index.js
@@ -1,4 +1,4 @@
-import 'expose?jQuery!expose?$!jquery';
-import 'expose?React!react';
+import 'expose?jQuery!expose?$!jquery'; // eslint-disable-line import/no-unresolved
+import 'expose?React!react'; // eslint-disable-line import/no-unresolved
 import './twitter-bootstrap';
 import './font-awesome';
