833dd45668a85c9916a3dd09c0d1db85b826ae5e
hung-phan <colorvisavn@gmail.com>
2016-06-18 00:05:03 +0700

    Add test for static page

--- tests/controllers/application.js
+++ tests/controllers/application.js
@@ -5,13 +5,23 @@ import app from 'app/server-app';
 describe('Controller: application', () => {
   const request = supertest(app.listen());
 
-  it('should render page', async () => {
+  it('should render Todos page', async () => {
     const result = await request
                           .get('/')
                           .set('Accept', 'text/html')
                           .expect(200);
 
     assert.ok(result.text);
-    assert.include(result.text, 'id="app"');
+    assert.include(result.text, 'Todos List');
+  });
+
+  it('should render Static page', async () => {
+    const result = await request
+                          .get('/static-page')
+                          .set('Accept', 'text/html')
+                          .expect(200);
+
+    assert.ok(result.text);
+    assert.include(result.text, 'Static Page');
   });
 });
