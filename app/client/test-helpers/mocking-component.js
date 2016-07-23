babc23957fb33eb5be69d71c4a5de341387b2828
hung-phan <colorvisavn@gmail.com>
2016-07-23 14:25:53 +0700

    Update mocking component

--- app/client/test-helpers/mocking-component.js
+++ app/client/test-helpers/mocking-component.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
 import React from 'react';
 
-export default (mockTagName, props = []) => () =>
-  (<div>{`${mockTagName} ${JSON.stringify(_.pick(this.props, props))}`}</div>);
+export default (mockTagName, props = []) => (componentProps) =>
+  (<div>{`${mockTagName} ${JSON.stringify(_.pick(componentProps, props))}`}</div>);
