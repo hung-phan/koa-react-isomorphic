a079da7850880be81f3a55f176e4e76c8d752de5
hung-phan <colorvisavn@gmail.com>
2016-07-23 14:21:46 +0700

    Add provider

---
+++ app/client/test-helpers/provider-mock.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class ProviderMock extends React.Component {
+  static propTypes = {
+    store: React.PropTypes.object,
+    children: React.PropTypes.oneOfType([
+      React.PropTypes.array,
+      React.PropTypes.object,
+    ]),
+  };
+
+  static childContextTypes = {
+    store: React.PropTypes.object.isRequired,
+  };
+
+  getChildContext() {
+    return { store: this.props.store };
+  }
+
+  render() {
+    return (<div>{ this.props.children }</div>);
+  }
+}
+
+export default ProviderMock;
