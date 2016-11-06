8247dcdafdae6ea8f3e4d53a4ffafbae252409d3
hung-phan <colorvisavn@gmail.com>
2016-11-06 19:15:05 +0700

    Update flowignore for prerender

--- app/server/middlewares/prerender.js
+++ app/server/middlewares/prerender.js
@@ -11,6 +11,7 @@ const networkLayer = new DefaultNetworkLayer(
 let routes = require('app/routes.jsx').default;
 
 if (module.hot) {
+  // $FlowFixMe
   module.hot.accept('./../../routes.jsx', () => {
     routes = require('./../../routes.jsx').default;
   });
