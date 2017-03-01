28812541003a84006b172f96b753bead25256cc0
hung-phan <colorvisavn@gmail.com>
2017-03-01 22:46:48 +0700

    Update inject-data-utils

--- app/client/helpers/inject-data-utils.jsx
+++ app/client/helpers/inject-data-utils.jsx
@@ -1,43 +1,47 @@
 import React from 'react';
 import Relay from 'react-relay';
-import ReactDOM from 'react-dom';
 import { trigger } from 'redial';
+import ReactDOM from 'react-dom';
 import { browserHistory as history, match } from 'react-router';
 import IsomorphicRelay from 'isomorphic-relay';
 import IsomorphicRouter from 'isomorphic-relay-router';
 import App from 'client/components/main/app';
-import navigateTo from './navigation';
 import { INJECT_PRELOAD_LINK_HOOK } from './redial-enhancer';
+import navigateTo from './navigation';
 
-export function prepareInitialRender(routes, domNode) {
-  const callback = location => match(
-    { routes, location },
-    (error, redirectLocation, renderProps) => {
-      if (error) {
-        navigateTo('/500.html');
-      } else if (redirectLocation) {
-        navigateTo(redirectLocation.pathname + redirectLocation.search);
-      } else if (renderProps) {
-        IsomorphicRouter.prepareInitialRender(Relay.Store, renderProps)
-          .then(props => {
-            ReactDOM.render(<App {...props} />, domNode);
-          });
+export const createHandler = callback => (error, redirectLocation, renderProps) => {
+  if (error) {
+    navigateTo('/500.html');
+  } else if (redirectLocation) {
+    navigateTo(redirectLocation.pathname + redirectLocation.search);
+  } else if (renderProps) {
+    callback(renderProps);
+  } else {
+    navigateTo('/404.html');
+  }
+};
 
-        trigger(INJECT_PRELOAD_LINK_HOOK, renderProps.components, renderProps);
-      } else {
-        navigateTo('/404.html');
-      }
-    }
-  );
+export const prepareInitialRender = (routes, domNode) => {
+  match({ routes, history }, createHandler(renderProps => {
+    IsomorphicRouter.prepareInitialRender(Relay.Store, renderProps)
+      .then(props => {
+        ReactDOM.render(<App {...props} />, domNode);
+      });
 
-  history.listen(callback);
-  callback(history.getCurrentLocation());
-}
+    trigger(INJECT_PRELOAD_LINK_HOOK, renderProps.components, renderProps);
+  }));
 
-export default function (routes, domNode) {
+  history.listen(location => {
+    match({ routes, location }, createHandler(renderProps => {
+      trigger(INJECT_PRELOAD_LINK_HOOK, renderProps.components, renderProps);
+    }));
+  });
+};
+
+export default (routes, domNode) => {
   if (process.env.SERVER_RENDERING) {
     IsomorphicRelay.injectPreparedData(Relay.Store, window.prerenderData);
   }
 
   prepareInitialRender(routes, domNode);
-}
+};
