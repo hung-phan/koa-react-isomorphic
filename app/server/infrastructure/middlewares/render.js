5a70a4567b3a2978e9df014d0752098522a5d1e2
hung-phan <colorvisavn@gmail.com>
2017-09-04 12:04:55 +0800

    Fix bug

--- app/server/infrastructure/middlewares/render.js
+++ app/server/infrastructure/middlewares/render.js
@@ -4,45 +4,47 @@ import path from "path";
 import marko from "marko";
 import settings from "../settings";
 
-export default async (ctx: Object, next: Function) => {
-  ctx.render = (
-    template: string,
-    parameters: Object = {},
-    initialState: Object = {}
-  ) => {
-    ctx.type = "text/html";
+export const render = (ctx: Object) => (
+  template: string,
+  parameters: Object = {},
+  initialState: Object = {}
+) => {
+  ctx.type = "text/html";
 
-    return new Promise((resolve, reject) => {
-      const templatePath = path.join(
-        settings.path.ROOT,
-        `${settings.path.TEMPLATES_DIR}/${template}`
-      );
-      const currentTemplate =
-        process.env.NODE_ENV === "production"
-          ? global.nodeRequire(`${templatePath}.js`)
-          : marko.load(templatePath);
+  return new Promise((resolve, reject) => {
+    const templatePath = path.join(
+      settings.path.ROOT,
+      `${settings.path.TEMPLATES_DIR}/${template}`
+    );
+    const currentTemplate =
+      process.env.NODE_ENV === "production"
+        ? global.nodeRequire(`${templatePath}.js`)
+        : marko.load(templatePath);
 
-      const builtParameters = {
-        ...parameters,
-        prerenderData: {
-          ...initialState,
-          ...parameters.prerenderData
-        }
-      };
-
-      try {
-        resolve(
-          currentTemplate.stream({
-            ...settings,
-            ...builtParameters,
-            csrf: ctx.csrf
-          })
-        );
-      } catch (e) {
-        reject(e);
+    const builtParameters = {
+      ...parameters,
+      prerenderData: {
+        ...initialState,
+        ...parameters.prerenderData
       }
-    });
-  };
+    };
+
+    try {
+      resolve(
+        currentTemplate.stream({
+          ...settings,
+          ...builtParameters,
+          csrf: ctx.csrf
+        })
+      );
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+export default async (ctx: Object, next: Function) => {
+  ctx.render = render(ctx);
 
   await next();
 };
