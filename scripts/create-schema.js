ba618f637a58d679973ef5b1caa3533e89ae98d7
hungphan <colorvisavn@gmail.com>
2018-03-07 11:29:24 +0800

    Fix build path bug

--- scripts/create-schema.js
+++ scripts/create-schema.js
@@ -4,20 +4,22 @@ import { graphql } from "graphql";
 import { introspectionQuery, printSchema } from "graphql/utilities";
 import schema from "../app/server/application/apis/graphqlSchema";
 
+const buildPath = path.join(__dirname, "../build");
+
 // Save JSON of full schema introspection for Babel Relay Plugin to use
-if (!fs.existsSync("../build")) {
-  fs.mkdirSync("../build");
+if (!fs.existsSync(buildPath)) {
+  fs.mkdirSync(buildPath);
 }
 
 graphql(schema, introspectionQuery).then(result => {
   fs.writeFileSync(
-    path.join(__dirname, "../build/schema.json"),
+    path.join(buildPath, "schema.json"),
     JSON.stringify(result, null, 2)
   );
 
   // Save user readable type system shorthand of schema
   fs.writeFileSync(
-    path.join(__dirname, "../build/schema.graphql"),
+    path.join(buildPath, "schema.graphql"),
     printSchema(schema)
   );
 });
--- scripts/update-schema.sh
+++ scripts/update-schema.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-if [ ! -f build/schema.graphql ]; then
-    mkdir -p build && touch build/schema.graphql
-fi
-
 babel-node ./scripts/create-schema.js
