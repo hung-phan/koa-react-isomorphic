e1d86321d082371320a8c37c7a42e43dcf5890e4
hungphan <colorvisavn@gmail.com>
2018-05-21 12:02:20 +0800

    Update server and default config for webpack

--- app/server.js
+++ app/server.js
@@ -6,7 +6,11 @@ import app from "./server/infrastructure/app";
 const PORT = process.env.PORT || 3000;
 
 export default () => {
-  http.createServer(app.callback()).listen(PORT, () => {
+  http.createServer(app.callback()).listen(PORT, error => {
+    if (error) {
+      throw error;
+    }
+
     console.info(`Server listening on port ${PORT}`); // eslint-disable-line no-console
   });
 };
