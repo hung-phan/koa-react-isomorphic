60b4eaaa1d527014a6efd04d198296a1aac2a166
hungphan <colorvisavn@gmail.com>
2018-06-10 22:16:43 +0800

    Update dependencies

--- app/routes.jsx
+++ app/routes.jsx
@@ -46,7 +46,7 @@ export const getRoutes = (
       getComponent={async (nextState, cb) => {
         const {
           default: StaticPageComponent
-        } = await import(/* webpackChunkName: "static-page" */ "./share/components/static-page");
+        } = await import(/* webpackChunkName: "static-page", webpackPrefetch: true */ "./share/components/static-page");
 
         cb(null, StaticPageComponent);
       }}
